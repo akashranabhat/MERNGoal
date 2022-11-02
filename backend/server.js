bfedd3c86230e45519f9a8c613d7686a48b0f978
Akash <akashranabhat05@gmail.com>
2022-11-02 14:30:45 +0545

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
 const port = process.env.port || 5001;
+
+connectDB();
 
 const app = express();
 
